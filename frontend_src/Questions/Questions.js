180e9aed20106ad81f1dc4c173379d3bb20531d5
Raynold Ng <raynold.ng24@gmail.com>
2020-05-12 17:55:40 +0800

    update frontend src files

--- frontend_src/Questions/Questions.js
+++ frontend_src/Questions/Questions.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-import { HOSTNAME } from '../contants'
+import { HOSTNAME } from '../constants'
 
 
 const NewQuestionCard = () => (
---
+++ frontend_src/constants.js
@@ -0,0 +1,2 @@
+export const HOSTNAME = "https://limitless-sands-43875.herokuapp.com"
+// export const HOSTNAME = "http://localhost:8081"
